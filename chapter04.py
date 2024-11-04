35c163b1ae73662f2b64e8e2edcd2f366bfb2811
ttfont <ttfont>
2024-11-04 17:47:04 +0800

    chapter04.py

---
+++ chapter04.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+
+@app.post("/items02/")
+async def create_item02(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items03/{item_id}")
+async def update_item03(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
+
+@app.put("/items04/{item_id}")
+async def update_item04(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
