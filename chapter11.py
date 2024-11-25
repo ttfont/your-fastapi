928fd4857d4ca203e225442cc52060ee62111ac9
ttfont <ttfont>
2024-11-25 15:02:21 +0800

    chapter11.py chapter12.py

---
+++ chapter11.py
@@ -0,0 +1,67 @@
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                }
+            ]
+        }
+    }
+
+
+class Item01(BaseModel):
+    name: str = Field(examples=["Foo"])
+    description: str | None = Field(default=None, examples=["A very nice Item"])
+    price: float = Field(examples=[35.4])
+    tax: float | None = Field(default=None, examples=[3.2])
+
+
+class Item02(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+@app.put("/items01/{item_id}")
+async def update_item(item_id: int, item: Item01):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+@app.put("/items02/{item_id}")
+async def update_item(
+        item_id: int,
+        item: Item02 = Body(
+            examples=[
+                {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                }
+            ],
+        ), ):
+    results = {"item_id": item_id, "item": item}
+    return results
