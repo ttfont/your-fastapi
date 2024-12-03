82b5d6339a856697d22633f2f0ae31a2baef4774
ttfont <ttfont>
2024-12-03 10:27:20 +0800

    chapter17.py

---
+++ chapter17.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, status, Response
+
+app = FastAPI()
+
+
+@app.post("/items/", status_code=201)
+async def create_item(name: str):
+    return {"name": name}
+
+
+@app.post("/items01/", status_code=status.HTTP_201_CREATED)
+async def create_item(name: str):
+    return {"name": name}
+
+
+tasks = {"foo": "Listen to the Bar Fighters"}
+
+
+@app.put("/get-or-create-task/{task_id}", status_code=200)
+def get_or_create_task(task_id: str, response: Response):
+    if task_id not in tasks:
+        tasks[task_id] = "This didn't exist before"
+        response.status_code = status.HTTP_201_CREATED
+    return tasks[task_id]
